0cd9b54b4a2c2fcc0cd494e18bcbf9dea32d02a1
qhinzzz <157957329@qq.com>
2021-05-18 21:36:30 +0800

    problems模块

---
+++ problems/src/test/java/com/hinz/problems/ProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinz.problems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
