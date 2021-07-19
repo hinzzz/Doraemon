fa9b1a7f26a1c7f72d27e7517ebfd252362ea02a
hinzzz <157957329@qq.com>
2021-07-19 23:13:41 +0800

    aop init

---
+++ aop/src/test/java/com/hinz/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinz.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
