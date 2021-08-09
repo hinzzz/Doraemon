4114a2efb4857833116712c3748934b145b4d7c4
hinzzz <157957329@qq.com>
2021-08-09 15:34:06 +0800

    es

--- elasticsearch/src/main/java/com/hinz/elasticsearch/bean/User.java
+++ elasticsearch/src/main/java/com/hinz/elasticsearch/bean/User.java
@@ -2,10 +2,12 @@ package com.hinz.elasticsearch.bean;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class User{
 	private String name;
